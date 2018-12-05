8ab2ae4ce52f34f50dba7e95207873f1abaafb5b
Eyal <eyal@robo-plan.com>
2018-12-05 09:00:54 +0000

    update

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
 from http.server import BaseHTTPRequestHandler
+import socketserver
+
+PORT = 8080
 
 class handler(BaseHTTPRequestHandler):
 
@@ -6,5 +9,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(str("Hello from Python on Now 2.0!").encode())
+        self.wfile.write((str("Hello from Python on Now 2.0!")+ str(self.path)).encode())
         return
+    
